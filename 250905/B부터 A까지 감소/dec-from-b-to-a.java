662a9e2c44b4db2df954f6fae72a5f46e2336b43
castlectry <122072831+newandthe@users.noreply.github.com>
2025-09-05 00:36:06 +0900

    Title: B부터 A까지 감소, Time: 87ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250905/B부터 A까지 감소/dec-from-b-to-a.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        for(int i=B; i>=A; i--) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
