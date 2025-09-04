ad7e9baff58383114d03c150512b3172d013a6c4
castlectry <122072831+newandthe@users.noreply.github.com>
2025-09-05 00:11:24 +0900

    Title: 물의 상태, Time: 88ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250905/물의 상태/state-of-water.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(n < 0 ? "ice" : (n >= 100 ? "vapor" : "water"));
+
+    }
+}
