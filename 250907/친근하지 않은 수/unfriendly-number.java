ebda9323d4dcc753f2d3690d7ab0b83cf0bd70cd
castlectry <122072831+newandthe@users.noreply.github.com>
2025-09-07 23:01:08 +0900

    Title: 친근하지 않은 수, Time: 87ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250907/친근하지 않은 수/unfriendly-number.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int divide2 = N/2;
+        int divide3 = N/3;
+        int divide5 = N/5;
+
+        int duplicationNumber = (N/6 + N/10 + N/15);
+
+        // System.out.println(divide2);
+        // System.out.println(divide3);
+        // System.out.println(divide5);
+        // System.out.println(duplicationNumber);
+
+        System.out.println(N - (divide2 + divide3 + divide5) + duplicationNumber);
+
+    }
+}
