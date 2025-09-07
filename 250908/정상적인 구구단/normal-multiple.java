4feb786274c21eeb94b9aaa11742abec6fa693a3
castlectry <122072831+newandthe@users.noreply.github.com>
2025-09-08 03:58:24 +0900

    Title: 정상적인 구구단, Time: 94ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250908/정상적인 구구단/normal-multiple.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int row=1; row<=N; row++) {
+            for(int col=1; col<=N; col++) {
+                System.out.print(row + " * " + col + " = " + row*col);
+
+                if(row != col) {
+                    System.out.print(", ");
+                }
+
+            }
+
+            System.out.println();
+
+        }
+
+    }
+}
