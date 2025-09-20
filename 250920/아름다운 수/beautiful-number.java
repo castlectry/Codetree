4df02e0a4288924bb607c4597fa1386d45e076cb
castlectry <122072831+newandthe@users.noreply.github.com>
2025-09-20 23:01:20 +0900

    Title: 아름다운 수, Time: 88ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 250920/아름다운 수/beautiful-number.java
+++ 250920/아름다운 수/beautiful-number.java
@@ -2,32 +2,21 @@ import java.io.*;
 
 public class Main {
 
-    static int N;       // 자리 수
-    static int result;  // 아름다운 수 개수
-
-    // depth: 현재까지 채운 자리 수
-    public static void beautyNumber(int depth) {
-        if (depth == N) {
-            result++;
-            return;
-        }
-        if (depth > N) return;
-
-        // 1부터 9까지 가능한 숫자
-        for (int d = 1; d <= 9; d++) {
-            // d를 d번 연속으로 채운다고 가정
-            beautyNumber(depth + d);
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        N = Integer.parseInt(br.readLine());
-        result = 0;
+        long[] dp = new long[N+1];
+        dp[0] = 1; // 길이 0은 "빈 수" 1가지
 
-        beautyNumber(0);
+        for (int len = 1; len <= N; len++) {
+            for (int d = 1; d <= 9; d++) {
+                if (len - d >= 0) {
+                    dp[len] += dp[len - d];
+                }
+            }
+        }
 
-        System.out.println(result);
+        System.out.println(dp[N]);
     }
 }
